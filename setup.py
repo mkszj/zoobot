6f0db76575e0cb36a9454cbb1db279d8aaa715cd
Mike Walmsley <walmsleymk1@gmail.com>
2025-05-21 19:01:23 -0400

    simplify installation

--- setup.py
+++ setup.py
@@ -21,83 +21,42 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages(),
     python_requires=">=3.9",  # bumped to 3.9 for typing
-    extras_require={
+
+    # fiddly
+    # extras_require={'foo': ['bar']},  # for testing only
+    # extras_require={'pytorch-cu128': ['torchvision']},
+
 
         # the cuda versions, for most users
 
         # the recommended/default version for most users
-        'pytorch-cu128': [  
-            'torch >= 2.7.0+cu128', # current latest
+
+            # 
+
+
+
+
+
+    extras_require={
+
+        'pytorch': [  
+            'torch >= 2.7.0',  # no longer requires cuda option
             'torchvision',
             'torchaudio',
             'torchmetrics',
             'lightning >= 2.2.5',
-            # 'albumentations < 2.0.0',
-            'pyro-ppl >= 1.8.6',
-            'timm >= 1.0.15'
-        ],   
-
-        # exactly as above, but _cu121 for cuda 12.8 (the current default)
-        'pytorch-cu121': [
-            'torch >= 2.3.1+cu121',  # older torch version
-            'torchvision',
-            'torchaudio',
-            'torchmetrics',
-            'lightning >= 2.2.5',
-            'albumentations < 2.0.0',
-            'pyro-ppl >= 1.8.6',
-            'timm >= 1.0.15'
-        ],     
-
-        # as above for cuda 11.8
-        # for GPU, you will also need e.g. cudatoolkit=11.3, 11.6
-        # https://pytorch.org/get-started/previous-versions/#v1121
-        'pytorch-cu118': [
-            'torch >= 2.3.1+cu118',   # older torch version
-            'torchvision',
-            'torchaudio',
-            'torchmetrics',
-            'lightning >= 2.2.5',
-            'albumentations < 2.0.0',
-            'pyro-ppl >= 1.8.6',
-            'timm >= 1.0.15'
-        ],  
-
-        # non-cuda options
-
-        'pytorch-cpu': [
-            # A100 GPU currently only seems to support cuda 11.3 on manchester cluster, let's stick with this version for now
-            # very latest version wants cuda 11.6
-            'torch >= 2.3.1+cpu',
-            'torchvision',
-            'torchaudio',
-            'torchmetrics',
-            'lightning >= 2.2.5',
-            # 'simplejpeg',
-            'albumentations < 2.0.0',
             'pyro-ppl >= 1.8.6',
             'timm >= 1.0.15'
         ],
-        'pytorch-m1': [
-            # as above but without the +cpu (and the extra-index-url in readme has no effect)
-            # all matching pytorch versions for an m1 system will be cpu
-            'torch >= 2.7.0',
-            'torchvision',
-            'torchaudio',
-            'torchmetrics',
-            'lightning >= 2.2.5',
-            'albumentations < 2.0.0',
-            'pyro-ppl >= 1.8.6',
-            'timm >= 1.0.15'
-        ],
+
         'pytorch-colab': [
             # colab includes pytorch already
             'lightning >= 2.2.5',
-            'albumentations < 2.0.0',
-            'pyro-ppl>=1.8.0',
-            'torchmetrics==0.14.0',
+            'torchmetrics',
+            'pyro-ppl>=1.8.6',
             'timm >= 1.0.15'
         ],
+        
         # TODO may add narval/Digital Research Canada config
 
         'utilities': [
@@ -112,6 +71,61 @@ setuptools.setup(
             'docutils<0.18',
             'sphinxemoji'
         ]
+
+        # exactly as above, but _cu121 for cuda 12.8 (the current default)
+        # 'pytorch-cu121': [
+        #     'torch >= 2.3.1+cu121',  # older torch version
+        #     'torchvision',
+        #     'torchaudio',
+        #     'torchmetrics',
+        #     'lightning >= 2.2.5',
+        #     'albumentations < 2.0.0',
+        #     'pyro-ppl >= 1.8.6',
+        #     'timm >= 1.0.15'
+        # ],     
+
+        # # as above for cuda 11.8
+        # # for GPU, you will also need e.g. cudatoolkit=11.3, 11.6
+        # # https://pytorch.org/get-started/previous-versions/#v1121
+        # 'pytorch-cu118': [
+        #     'torch >= 2.3.1+cu118',   # older torch version
+        #     'torchvision',
+        #     'torchaudio',
+        #     'torchmetrics',
+        #     'lightning >= 2.2.5',
+        #     'albumentations < 2.0.0',
+        #     'pyro-ppl >= 1.8.6',
+        #     'timm >= 1.0.15'
+        # ],  
+
+        # non-cuda options
+
+        # 'pytorch-cpu': [
+        #     # A100 GPU currently only seems to support cuda 11.3 on manchester cluster, let's stick with this version for now
+        #     # very latest version wants cuda 11.6
+        #     'torch >= 2.3.1+cpu',
+        #     'torchvision',
+        #     'torchaudio',
+        #     'torchmetrics',
+        #     'lightning >= 2.2.5',
+        #     # 'simplejpeg',
+        #     'albumentations < 2.0.0',
+        #     'pyro-ppl >= 1.8.6',
+        #     'timm >= 1.0.15'
+        # ],
+        # 'pytorch-m1': [
+        #     # as above but without the +cpu (and the extra-index-url in readme has no effect)
+        #     # all matching pytorch versions for an m1 system will be cpu
+        #     'torch >= 2.7.0',
+        #     'torchvision',
+        #     'torchaudio',
+        #     'torchmetrics',
+        #     'lightning >= 2.2.5',
+        #     'albumentations < 2.0.0',
+        #     'pyro-ppl >= 1.8.6',
+        #     'timm >= 1.0.15'
+        # ],
+
     },
     install_requires=[
         'h5py',
